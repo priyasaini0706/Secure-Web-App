f06ea23201ff2f823c8866f49088d13c20d06fc7
Priya Saini <priyasaini0706@gmail.com>
2026-04-15 01:42:43 +0100

    fix dotenv loading order

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
+console.log("ENV TEST:");
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_PORT =", process.env.DB_PORT);
+
 const http = require("http");
 
 const app = require("./app");
-// const connectDB = require("./config/database");
 
 const PORT = process.env.PORT || 8080;
-
-// connectDB();
 
 const server = http.createServer(app);
 
