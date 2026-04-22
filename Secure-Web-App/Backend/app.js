488e301b141dbcf8a61f4754caebf656f70b6753
Priya Saini <priyasaini0706@gmail.com>
2026-04-22 23:36:39 +0100

    server update

--- Secure-Web-App/Backend/app.js
+++ Secure-Web-App/Backend/app.js
@@ -12,7 +12,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://securewebapp.netlify.app"
+  "https://secure-web-app.netlify.app",
+  "https://secure-web-app-production-d271.up.railway.app"
 ];
 
 const corsOptions = {
@@ -54,9 +55,9 @@ app.set("trust proxy", 1);
 // ✅ Define allowed origins
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://securewebapp.netlify.app"
+  "https://secure-web-app.netlify.app",
+  "https://secure-web-app-production-d271.up.railway.app"
 ];
-
 
 
 // middlewares
